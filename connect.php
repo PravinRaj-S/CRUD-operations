c9ede82cc1b8125ffd46744b68d794c5013c2755
Pravin Raj S <102790780+PravinRaj-S@users.noreply.github.com>
2022-04-02 07:30:30 -0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$con = new mysqli('localhost','root','','crudoperation');
+
+
+if(!$con)
+{
+    die(mysqli_error($con));
+}
+
+/*
+if($con)
+{
+    echo "connection successfull";
+}
+else
+{
+    die(mysqli_error($con));
+}*/
+
+
+
+?>
